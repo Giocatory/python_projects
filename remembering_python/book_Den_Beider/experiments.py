48cb768cbc88e72ed95e85a851960327e3a27dec
Giocatory <nivolyas@gmail.com>
2023-05-23 23:37:14 +0800

    experiments

--- remembering_python/book_Den_Beider/experiments.py
+++ remembering_python/book_Den_Beider/experiments.py
@@ -1,3 +1,8 @@
-print(
-    (1245**13 + 20**5**3 - 40)*400
-)
+minutes, start_hours, start_minutes = int(input()), int(input()), int(input())
+
+res = start_hours * 60 + start_minutes + minutes
+res_h = res // 60
+res_m = res % 60
+while res_h > 24:
+    res_h -= 24
+print(res_h, res_m)

e958ac77bab0f3538b6c0576061a0f0049d8574c
Giocatory <nivolyas@gmail.com>
2023-05-25 21:34:24 +0800

    for experiments

--- remembering_python/book_Den_Beider/experiment.py
+++ remembering_python/book_Den_Beider/experiment.py
@@ -1,11 +1,1 @@
-match t := input():
-    case "Flask" | "Django" | "Fast-API":
-        print(f"Python({t}),Backend-dev")
-    case "Angular" | "React" | "Vue":
-        print(f"JavaScript/TypeScript({t}),Frontend-dev")
-    case "Flutter" | "React Native":
-        print(f"JavaScript({t}),Cross-Mobile-dev")
-    case "Pandas" | "skit-learn" | "keras":
-        print(f"Python({t}),Data-Scientist")
-    case _:
-        print("модель еще не обучена")
+print(*[i+"!" for i in input().split(" ")], sep="\n")

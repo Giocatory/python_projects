93cc68b47320409a78550ceaa66c8b5666928c49
Giocatory <nivolyas@gmail.com>
2023-05-29 19:50:48 +0800

    work with string

---
+++ remembering_python/book_Den_Beider/all data type/Strings/str type.py
@@ -0,0 +1,21 @@
+# Литералы строк
+var1: str = " "
+var2: str = ' '
+var3: str = """ """
+var4: str = ''' '''
+
+# Concatenation
+var1 = "One"
+var2 = "Two"
+print(var1 + " " + var2)  # One Two
+
+# Indexing
+print(var1[1])  # n
+
+# Slicing
+var3 = "Hello user"
+print(var3[0:5])  # Hello
+
+# Длинна
+var4 = "abcdefghijklmnopqrstuvwxyz"  # 26
+print(len(var4))

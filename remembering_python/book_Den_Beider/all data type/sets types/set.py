1f9c6fbc60bf8b58946c1b434a8497941fc185cd
Giocatory <nivolyas@gmail.com>
2023-05-30 21:54:01 +0800

    work with sets

--- remembering_python/book_Den_Beider/all data type/sets types/set.py
+++ remembering_python/book_Den_Beider/all data type/sets types/set.py
@@ -3,9 +3,9 @@
 
 arr = ["Mike", "Tatyana", "Tom", "Mike", "Tom", "Tom", "Tatyana"]
 set_arr = set(arr)
-print( set_arr )  # {'Mike', 'Tatyana', 'Tom'}
-print( len(set_arr) ) # 3
-print( "Tatyana" in set_arr )  # True
+print(set_arr)  # {'Mike', 'Tatyana', 'Tom'}
+print(len(set_arr))  # 3
+print("Tatyana" in set_arr)  # True
 if "Tom" in set_arr:
     set_arr.remove("Tom")
-print( set_arr )  # {'Mike', 'Tatyana'}
+print(set_arr)  # {'Mike', 'Tatyana'}

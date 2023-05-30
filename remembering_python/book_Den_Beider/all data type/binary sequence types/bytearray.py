cb4a24b91d42a2a9ea9f818bb450cfede6ece8e1
Giocatory <nivolyas@gmail.com>
2023-05-30 21:53:15 +0800

    work with bytes

--- remembering_python/book_Den_Beider/all data type/binary sequence types/bytearray.py
+++ remembering_python/book_Den_Beider/all data type/binary sequence types/bytearray.py
@@ -2,11 +2,12 @@
 b_one: bytearray = bytearray("taxi", "utf-8")
 b_two: bytearray = bytearray("🤦‍♀️👌🤷‍♂️😉😎 emoji", "utf-16", errors="ignore")
 
-print( b_one )  # bytearray(b'taxi')
-print( b_two )  # bytearray(b'\xff\xfe>\xd8&\xdd\r @&\x0f\xfe=\xd8L\xdc>\xd87\xdd\r B&\x0f\xfe=\xd8\t\xde=\xd8\x0e\xde \x00e\x00m\x00o\x00j\x00i\x00')
+print(b_one)  # bytearray(b'taxi')
+print(b_two)  # bytearray(b'\xff\xfe>\xd8&\xdd\r @&\x0f\xfe=\xd8L\xdc>\xd87\xdd\r
+# B&\x0f\xfe=\xd8\t\xde=\xd8\x0e\xde \x00e\x00m\x00o\x00j\x00i\x00')
 
-print( b_one[2] )  # 120
-print( str(b_one, "ср866") )  # taxi
+print(b_one[2])  # 120
+print(str(b_one, "ср866"))  # taxi
 
 ###################### Methods ######################
 
@@ -14,4 +15,4 @@ print( str(b_one, "ср866") )  # taxi
 b_to_app = bytearray('str', 'ascii')
 for i in list(bytes("ing", 'ascii')):
     b_to_app.append(i)
-print( b_to_app )
+print(b_to_app)

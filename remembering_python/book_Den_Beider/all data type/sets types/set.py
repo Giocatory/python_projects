6a14918c77f03000c50ffa72a92588a4ea188da1
Mikhail Derkunov <36039811+Giocatory@users.noreply.github.com>
2023-05-30 11:41:35 +0300

    work with set

---
+++ remembering_python/book_Den_Beider/all data type/sets types/set.py
@@ -0,0 +1,11 @@
+# create
+# {1, 2}, set()
+
+arr = ["Mike", "Tatyana", "Tom", "Mike", "Tom", "Tom", "Tatyana"]
+set_arr = set(arr)
+print( set_arr )  # {'Mike', 'Tatyana', 'Tom'}
+print( len(set_arr) ) # 3
+print( "Tatyana" in set_arr )  # True
+if "Tom" in set_arr:
+    set_arr.remove("Tom")
+print( set_arr )  # {'Mike', 'Tatyana'}

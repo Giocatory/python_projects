ef7cf266149e312b764b3af852454fe8cbff66b7
Mikhail Derkunov <36039811+Giocatory@users.noreply.github.com>
2023-05-31 10:33:48 +0800

    work with dict

--- remembering_python/book_Den_Beider/all data type/comparison type/dict type.py
+++ remembering_python/book_Den_Beider/all data type/comparison type/dict type.py
@@ -15,3 +15,31 @@ d7 = dict(
     ]
 )  # {'one': 1, 'two': 3, 'three': 3}
 print(d1,d2,d3,d4,d5,d6,d7,sep="\n")
+
+# обращение по ключу, если ключа нет, то создастся новый
+# Но если мы попробуем получить значение с ключом, которого нет в словаре, то Python сгенерирует ошибку KeyError
+view_dict: dict = {
+    "Father": "Mikhail",
+    "Mother": "Tatyana",
+    "Children": {
+        "first": "",
+        "second": ""
+    }
+}
+
+view_dict["Children"]["first"] = "Margo"
+view_dict["Children"]["second"] = "Varvar"
+view_dict["Children"]["third"] = "Avro"
+
+print(view_dict["Children"]["third"])  # Avro
+# print(view_dict["salary"])  произойдет исключение [KeyError]
+if "salary" in view_dict.keys():
+    print(view_dict["salary"])
+else:
+    print("This key not found")  # This key not found
+
+for k, v in view_dict.items():
+    print(f"{k}: {v}")
+# Father: Mikhail
+# Mother: Tatyana
+# Children: {'first': 'Margo', 'second': 'Varvar', 'third': 'Avro'}

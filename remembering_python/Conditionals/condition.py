2e84426389450f77cc0fa496488c10d4442f1bed
Mikhail Derkunov <36039811+Giocatory@users.noreply.github.com>
2023-06-04 13:19:49 +0800

    work with conditions

---
+++ remembering_python/Conditionals/condition.py
@@ -0,0 +1,32 @@
+# Syntaxes
+
+"""
+1) 
+if condition:
+    instructions
+else:
+    instructions
+    
+2)
+if condition 1:
+    instructions
+elif condition 2:
+    instructions
+else:
+    instructions
+
+
+3)
+color = input()
+match color:
+    case 'red' | 'yellow'
+        print('Stop')
+    case 'green'
+        print('May go')
+    case _:
+        print('Invalid value')
+
+
+4)
+Instruction 1 if condition else Instruction 2
+"""

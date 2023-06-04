35b3a3d791446c2815f8b1a6ac3d97a296ca5ab5
Mikhail Derkunov <36039811+Giocatory@users.noreply.github.com>
2023-06-04 15:38:09 +0800

    work with functions

--- remembering_python/book_Den_Beider/Functions/with_parameters.py
+++ remembering_python/book_Den_Beider/Functions/with_parameters.py
@@ -25,3 +25,26 @@ user("Mike", 35)  # Hello Mike. Welcome to company
 user("Mike")  # Hello Mike. You don't enter age!
 user(age=35)  # Hello unknown-user. Please enter name
 user()  # Hello unknown-user. You don't enter age!
+
+# Не определенное количество параметров
+
+# returned tuple
+def undef1(*args):
+    return args
+
+print(undef1(1,2,3,4,5))  # (1, 2, 3, 4, 5)
+print(undef1([1,2]))  # ([1, 2],)
+
+def undef2(*args):
+    l = []
+    l.extend(args)
+    return l
+
+print(undef2(1,2,3,4,5))  # [1, 2, 3, 4, 5]
+
+# returned dict
+def undef3(**kwargs):
+    return kwargs
+
+print(undef3(small="[14;18)", norm="[18;55)", old="[55;100]"))
+# {'small': '[14;18)', 'norm': '[18;55)', 'old': '[55;100]'}

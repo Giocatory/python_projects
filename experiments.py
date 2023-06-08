e24971bec7aa8e5600ddae6a2aab6593a248e297
Giocatory <nivolyas@gmail.com>
2023-06-08 23:35:20 +0800

    experiments

--- experiments.py
+++ experiments.py
@@ -1,13 +1,3 @@
-s = "+71234567890 +71234567854 +61234576890 +52134567890 +21235777890 +21234567110 +71232267890".split(" ")
-s.sort()
-
-s = [[i[0:2], i[2::]] for i in s]
-d = {}
-
-for i in s:
-    if i[0] not in d:
-        d[i[0]] = [i[1]]
-    else:
-        d[i[0]].append(i[1])
-
-print(*sorted(d.items()),sep="\n")
+s = "Петя Варвара Венера Василиса Василий Федор"
+r = tuple(filter(lambda x: "ва" in x, s.lower().split(" ")))
+print(*r)

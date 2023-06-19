36306d40e037c027af20af295dddf91e4d5b8eeb
Giocatory <nivolyas@gmail.com>
2023-06-20 00:30:06 +0800

    20.06.2023

--- remembering_python/work_with_modules/module_csv.py
+++ remembering_python/work_with_modules/module_csv.py
@@ -8,11 +8,11 @@ daily_temperatures = [
     [61, 60, 66, 73, 59, 65],
 ]
 
-dir_path = '/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules/documents'
+dir_path = '/media/giocatory/hard_out_disk/python_projects/remembering_python/work_with_modules/documents'
 if not os.path.exists(dir_path):
     os.mkdir(dir_path)
 
-file_path = "/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules/documents/doc.csv"
+file_path = "/media/giocatory/hard_out_disk/python_projects/remembering_python/work_with_modules/documents/doc.csv"
 if not os.access(file_path, mode=os.F_OK):
     os.system(f"touch {file_path}")
 
@@ -31,3 +31,12 @@ with open(file_path, 'r') as file:
 # ['68', '65', '71', '57', '62', '58']
 # ['57', '60', '66', '73', '59', '60']
 # ['61', '60', '66', '73', '59', '65']
+
+with open(file_path, 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row)
+
+# {'Monday': '68', 'Tuesday': '65', 'Wednesday': '71', 'Thursday': '57', 'Friday': '62', 'Saturday': '58'}
+# {'Monday': '57', 'Tuesday': '60', 'Wednesday': '66', 'Thursday': '73', 'Friday': '59', 'Saturday': '60'}
+# {'Monday': '61', 'Tuesday': '60', 'Wednesday': '66', 'Thursday': '73', 'Friday': '59', 'Saturday': '65'}

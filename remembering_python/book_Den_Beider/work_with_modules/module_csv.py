126950173c667ace3f531a443c26802261f54082
Giocatory <nivolyas@gmail.com>
2023-06-20 00:02:40 +0800

    csv module

--- remembering_python/book_Den_Beider/work_with_modules/module_csv.py
+++ remembering_python/book_Den_Beider/work_with_modules/module_csv.py
@@ -5,11 +5,29 @@ import csv
 daily_temperatures = [
     [68, 65, 71, 57, 62, 58],
     [57, 60, 66, 73, 59, 60],
-    [61, 60, 66, 73, 59, 60],
+    [61, 60, 66, 73, 59, 65],
 ]
 
-if not os.path.exists('/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules'):
-    os.mkdir('/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules')
+dir_path = '/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules/documents'
+if not os.path.exists(dir_path):
+    os.mkdir(dir_path)
 
-if not os.access("/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules/documents/doc.csv", mode=os.F_OK):
-    os.system("touch /media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules/documents/doc.csv")
+file_path = "/media/giocatory/hard_out_disk/python_projects/remembering_python/book_Den_Beider/work_with_modules/documents/doc.csv"
+if not os.access(file_path, mode=os.F_OK):
+    os.system(f"touch {file_path}")
+
+
+with open(file_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
+    writer.writerows(daily_temperatures)
+
+with open(file_path, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+# ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+# ['68', '65', '71', '57', '62', '58']
+# ['57', '60', '66', '73', '59', '60']
+# ['61', '60', '66', '73', '59', '65']

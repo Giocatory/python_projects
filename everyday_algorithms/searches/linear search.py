d18c53684d9e39646935753681d50b111b13034a
Giocatory <nivolyas@gmail.com>
2023-06-21 21:37:52 +0800

    линейный поиск

---
+++ everyday_algorithms/searches/linear search.py
@@ -0,0 +1,15 @@
+def linearSearch(array, value):
+    index = 0
+    found = False
+    while index < len(array) and not found:
+        if array[index] == value:
+            found = True
+        else:
+            index += 1
+    
+    return index if found else -1
+
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+print(linearSearch(arr, 3))  # 2
+print(linearSearch(arr, 8))  # -1

735d536cdaf85fbe3353e8429dc3af471fab580e
Giocatory <nivolyas@gmail.com>
2023-06-25 17:34:39 +0800

    создание последовательности кортежей со всеми возможными перестановками элементов

--- gaining experience/mini tasks/combinations.py
+++ gaining experience/mini tasks/combinations.py
@@ -1,6 +1,6 @@
 # Вы хотите проитерировать по всем возможным комбинациям и
 # перестановкам коллекции элементов.
-from itertools import permutations
+from itertools import combinations_with_replacement, permutations
 
 items = ['a', 'b', 'c', 'd']
 
@@ -49,3 +49,43 @@ for comb in permutations(items, 2):
 # ('d', 'a')
 # ('d', 'b')
 # ('d', 'c')
+
+# или с соединением элемента комбинациями (к примеру)
+for comb in combinations_with_replacement(items, 4):
+    print(comb)
+
+# ('a', 'a', 'a', 'a')
+# ('a', 'a', 'a', 'b')
+# ('a', 'a', 'a', 'c')
+# ('a', 'a', 'a', 'd')
+# ('a', 'a', 'b', 'b')
+# ('a', 'a', 'b', 'c')
+# ('a', 'a', 'b', 'd')
+# ('a', 'a', 'c', 'c')
+# ('a', 'a', 'c', 'd')
+# ('a', 'a', 'd', 'd')
+# ('a', 'b', 'b', 'b')
+# ('a', 'b', 'b', 'c')
+# ('a', 'b', 'b', 'd')
+# ('a', 'b', 'c', 'c')
+# ('a', 'b', 'c', 'd')
+# ('a', 'b', 'd', 'd')
+# ('a', 'c', 'c', 'c')
+# ('a', 'c', 'c', 'd')
+# ('a', 'c', 'd', 'd')
+# ('a', 'd', 'd', 'd')
+# ('b', 'b', 'b', 'b')
+# ('b', 'b', 'b', 'c')
+# ('b', 'b', 'b', 'd')
+# ('b', 'b', 'c', 'c')
+# ('b', 'b', 'c', 'd')
+# ('b', 'b', 'd', 'd')
+# ('b', 'c', 'c', 'c')
+# ('b', 'c', 'c', 'd')
+# ('b', 'c', 'd', 'd')
+# ('b', 'd', 'd', 'd')
+# ('c', 'c', 'c', 'c')
+# ('c', 'c', 'c', 'd')
+# ('c', 'c', 'd', 'd')
+# ('c', 'd', 'd', 'd')
+# ('d', 'd', 'd', 'd')

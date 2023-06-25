8548d0e800ad63609428bdd7b7a1cf583da9f6fe
Giocatory <nivolyas@gmail.com>
2023-06-25 16:14:43 +0800

    создать список N максимальных или минимальных элементов коллекции.

---
+++ gaining experience/mini tasks/min-max-elements.py
@@ -0,0 +1,34 @@
+# Вы хотите создать список N максимальных или минимальных элементов коллекции.
+import heapq
+
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+
+print(heapq.nlargest(3, nums))  # [42, 37, 23]
+print(heapq.nsmallest(3, nums))  # [-4, 1, 2]
+
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+
+print(cheap)
+# [
+    # {'name': 'YHOO', 'shares': 45, 'price': 16.35}, 
+    # {'name': 'FB', 'shares': 200, 'price': 21.09}, 
+    # {'name': 'HPQ', 'shares': 35, 'price': 31.75}
+# ]
+print(expensive)
+# [
+    # {'name': 'AAPL', 'shares': 50, 'price': 543.22}, 
+    # {'name': 'ACME', 'shares': 75, 'price': 115.65}, 
+    # {'name': 'IBM', 'shares': 100, 'price': 91.1}
+# ]

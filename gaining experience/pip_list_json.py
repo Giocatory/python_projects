a363a875a9efb5ccdf079d84e486b6af8084dcfe
Giocatory <nivolyas@gmail.com>
2023-07-03 22:20:31 +0800

    experiment

--- gaining experience/pip_list_json.py
+++ gaining experience/pip_list_json.py
@@ -1,7 +1,7 @@
 import os
 
 json_text = os.system("pip list --format=json")
-print(type(json_text))
+print(type(json_text))  # int
 
 # [{"name": "annotated-types", "version": "0.5.0"}, {"name": "pip", "version": "23.1.2"}, {"name": "pydantic",
 # "version": "2.0"}, {"name": "pydantic_core", "version": "2.0.1"}, {"name": "setuptools", "version": "68.0.0"},
